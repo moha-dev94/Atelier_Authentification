bd787e3d148b5269a2eb5c36a3a93486ea8af94b
moha-dev94 <ennadifima@gmail.com>
2025-11-05 10:31:53 +0100

    Update cookie validation to use authToken

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -4,7 +4,7 @@ session_start();
 $randomNumber = $_SESSION['randomNumber'];
 // Vérifier si l'utilisateur est bien en possession d'un cookie valide
 // Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (!isset($_COOKIE['randomNumber']) || $_COOKIE['randomNumber'] !== 'randomNumber') {
+if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== $randomNumber) {
     header('Location: index.php');
     exit();
 }
