4811c49ac4b2659bea5cdad7b7b6c7ad966e6051
moha-dev94 <ennadifima@gmail.com>
2025-11-05 11:02:06 +0100

    Fix session handling in logout.php
    Fix missing semicolon and clean up logout logic.

--- atelier2/logout.php
+++ atelier2/logout.php
@@ -1,7 +1,6 @@
 <?php
 // Supprime le cookie d'authentification
 session_start()
-  $randomNumber = $_SESSION['randomNumber'];
 setcookie('authToken', '', time() - 60, '/', '', false, true); // Cookie expiré par la valeur -3600. Il n'est donc plus valable
 
 // Redirection vers la page de connexion
