6ad682f36c91f8c9805665578fc347f068e3c45f
moha-dev94 <ennadifima@gmail.com>
2025-11-05 10:26:51 +0100

    Fix session variable retrieval for randomNumber
    Retrieve random number from session variable.

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -1,7 +1,7 @@
 <?php
 // Démarrer la session
 session_start();
-$_SESSION['randomNumber'];
+$randomNumber = $_SESSION['randomNumber'];
 // Vérifier si l'utilisateur est bien en possession d'un cookie valide
 // Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
 if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== 'randomNumber') {
