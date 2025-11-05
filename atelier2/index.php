ca2cd90aa6da1b4af3a101455cced82af288eab2
moha-dev94 <ennadifima@gmail.com>
2025-11-05 10:16:25 +0100

    Correct random number assignment for authToken check
    Fix random number assignment for cookie validation.

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,11 +1,11 @@
 <?php
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
-
+$randomNumber === bin2hex(random_bytes(16))
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
-$randomNumber === bin2hex(random_bytes(16))
+
 if (isset($_COOKIE['authToken']) && $_COOKIE['authToken'] === $randomNumber) {
     header('Location: page_admin.php');
     exit();
